f3b120b4621360bb4766076d3d641e08328b2660
added0ace <added0ace@gmail.com>
2026-03-16 09:59:00 +0500

    Update latest.js

--- api/latest.js
+++ api/latest.js
@@ -1,18 +1,11 @@
+import { getRedirect } from "../lib/redirect.js";
+
 export default async function handler(req, res) {
 
-  const r = await fetch("https://dnull.xyz/latest_bs", {
-    redirect: "follow"
-  });
-
-  const fileName = new URL(r.url).pathname.split("/").pop();
-
-  const target =
-    "https://tempweb.nullsusercontent.com/fpapk/" +
-    fileName +
-    "?allowUnsignedMods=1";
+  const url = await getRedirect();
 
   res.writeHead(302, {
-    Location: target
+    Location: url
   });
 
   res.end();
