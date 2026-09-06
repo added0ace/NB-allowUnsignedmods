c9967781b636a5ad58502265140177eafeff60e1
added0ace <added0ace@gmail.com>
2026-09-06 20:41:38 +0500

    Refactor redirect logic to use getRedirect function

--- redirect.js
+++ redirect.js
@@ -1,38 +1,12 @@
-async function p(u) {
-  const t = performance.now();
-  try {
-    await fetch(u, { method: 'HEAD', mode: 'no-cors' });
-    return performance.now() - t;
-  } catch (e) {
-    return Infinity;
-  }
-}
+import { getRedirect } from "./lib/redirect.js";
 
 async function go() {
   try {
-    const h = [
-      "https://tempweb.nullsusercontent.com",
-      "https://anticensorship.nullsusercontent.com"
-    ];
-    const r = await Promise.all(h.map(x => p(x)));
-    const f = r[0] <= r[1] ? h[0] : h[1];
-    const o = f === Infinity ? h[0] : f;
-    let s = "";
-    try {
-      const d = await fetch("https://dnull.xyz/latest_bs", { redirect: "follow" });
-      s = new URL(d.url).pathname.split("/").pop();
-    } catch (e) {
-      try {
-        const g = await fetch("./version.json");
-        const j = await g.json();
-        s = j.file;
-      } catch (ex) {
-        s = "nb_67.264_release_863546cb.apk";
-      }
-    }
-    window.location.replace(o.includes("tempweb") ? `${o}/fpapk/${s}?allowUnsignedMods=1` : `${o}/${s}?allowUnsignedMods=1`);
+    const url = await getRedirect();
+    window.location.replace(url);
   } catch (err) {
     document.body.innerText = "Error: " + err;
   }
 }
+
 go();
